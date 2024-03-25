84bc2e924c2ad998999f65a8b3ed21df9420314d
Sergii <apserbud22@gmail.com>
2024-03-25 15:38:02 +0200

    small change

--- src/Pages/UserProfilePage.tsx
+++ src/Pages/UserProfilePage.tsx
@@ -44,7 +44,7 @@ export const UserProfilePage = memo(() => {
         {targetUser && (
           <>
             <UserItem user={targetUser} />
-            {isChangeable || (targetUser.is_superuser && <UpdateUserForm />)}
+            {(isChangeable || targetUser.is_superuser) && <UpdateUserForm />}
           </>
         )}
       </GenericAuthContent>
