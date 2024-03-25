7713dfd0d28d906cd0d5084993a13a876ad278c2
Sergii <apserbud22@gmail.com>
2024-03-25 15:36:50 +0200

    add superUser

--- src/Pages/UserProfilePage.tsx
+++ src/Pages/UserProfilePage.tsx
@@ -44,7 +44,7 @@ export const UserProfilePage = memo(() => {
         {targetUser && (
           <>
             <UserItem user={targetUser} />
-            {isChangeable && <UpdateUserForm />}
+            {isChangeable || (targetUser.is_superuser && <UpdateUserForm />)}
           </>
         )}
       </GenericAuthContent>
